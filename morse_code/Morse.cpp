739fdb9af1032a493a275502689b7dd6ba868333
Andrew Burton <tuglyraisin@gmail.com>
2014-05-14 16:37:25 -0500

    Just need to debug.

--- morse_code/Morse.cpp
+++ morse_code/Morse.cpp
@@ -7,32 +7,39 @@ Morse::Morse(int led) {
   boolean bye = false;
   this->led = led;
   this->unit = 100;
+  
+  // Setup the pin
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
+  // Table is derived from:
+  // http://upload.wikimedia.org/wikipedia/commons/b/b5/International_Morse_Code.svg
   char code[37][7] = {{'A', '.', '-', '\0'},
                       {'B', '-', '.', '.', '.', '\0'},
-                      {'C', '\0'},
-                      {'D', '\0'},
-                      {'E', '\0'},
-                      {'F', '\0'},
-                      {'G', '\0'},
-                      {'H', '\0'},
-                      {'I', '\0'},
-                      {'J', '\0'},
-                      {'K', '\0'},
-                      {'L', '\0'},
-                      {'M', '\0'},
-                      {'N', '\0'},
-                      {'O', '\0'},
-                      {'P', '\0'},
-                      {'Q', '\0'},
-                      {'R', '\0'},
-                      {'S', '\0'},
-                      {'T', '\0'},
-                      {'U', '\0'},
-                      {'V', '\0'},
-                      {'W', '\0'},
-                      {'X', '\0'},
-                      {'Y', '\0'},
-                      {'Z', '\0'},
+                      {'C', '-', '.', '-', '.', '\0'},
+                      {'D', '-', '.', '.', '\0'},
+                      {'E', '.', '\0'},
+                      {'F', '.', '.', '-', '.', '\0'},
+                      {'G', '-', '-', '.', '\0'},
+                      {'H', '.', '.', '.', '.', '\0'},
+                      {'I', '.', '.', '\0'},
+                      {'J', '.', '-', '-', '-', '\0'},
+                      {'K', '-', '.', '-', '\0'},
+                      {'L', '.', '-', '.', '.', '\0'},
+                      {'M', '-', '-', '\0'},
+                      {'N', '-', '.', '\0'},
+                      {'O', '-', '-', '-', '\0'},
+                      {'P', '.', '-', '-', '.', '\0'},
+                      {'Q', '-', '-', '.', '-', '\0'},
+                      {'R', '.', '-', '.', '\0'},
+                      {'S', '.', '.', '.', '\0'},
+                      {'T', '-', '\0'},
+                      {'U', '.', '.', '-', '\0'},
+                      {'V', '.', '.', '.',  '-', '\0'},
+                      {'W', '.', '-', '-', '\0'},
+                      {'X', '-', '.', '-', '-', '\0'},
+                      {'Y', '-', '.', '-', '-', '\0'},
+                      {'Z', '-', '-', '.', '.', '\0'},
                       {'1', '.', '-', '-', '-', '-', '\0'},
                       {'2', '.', '.', '-', '-', '-', '\0'},
                       {'3', '.', '.', '.', '-', '-', '\0'},
@@ -43,7 +50,7 @@ Morse::Morse(int led) {
                       {'8', '-', '-', '-', '.', '.', '\0'},
                       {'9', '-', '-', '-', '-', '.', '\0'},
                       {'0', '-', '-', '-', '-', '-', '\0'},
-                      {' ', '-', '-', '.', '\0'}};
+                      {' ', '\0'}};
 
   // Copy to the attribute
   for(int x=0;x<2;x++) {
@@ -53,16 +60,40 @@ Morse::Morse(int led) {
     }
     if (bye == true) { break; }
   }
-  /*
-
-   };*/
 }
 
-Morse::~Morse() { 
-}
+Morse::~Morse() { }
 
 void Morse::say(char letters[255]) {
-
+  for(int r = 0; r < 255; r++) {
+    if (letters[r] == '\0') break;
+    char letter = letters[r];
+    
+    if (letter == ' ') {
+      delay(this->unit * 7);
+    }
+    else {
+      int ltr = 100;
+      int flashType = 1;
+      for(int chr = 0; chr < 36; chr++) {
+        if (this->code[chr][0] == letter) {
+          ltr = chr;
+          break;
+        }
+      }
+      Serial.print(this->code[ltr][0]);
+      while(this->code[ltr][flashType] != '\0') {
+        Serial.print(this->code[ltr][flashType]);
+        delay(this->unit);
+        if (this->code[ltr][flashType] == '.') dot();
+        if (this->code[ltr][flashType] == '-') dash();
+        flashType++;
+      }
+    }
+    
+    // Space between letter
+    delay(this->unit * 3);
+  }
 }
 
 void Morse::setUnit(int t) {
@@ -77,7 +108,7 @@ void Morse::dash() {
 
 void Morse::dot() {
   digitalWrite(this->led, HIGH);
-  delay(this->unit * 3);
+  delay(this->unit);
   digitalWrite(this->led, LOW);
 }
 
--- morse_code/morse_code.ino
+++ morse_code/morse_code.ino
@@ -1,11 +1,48 @@
 /*****
  * MORSE CODE
  *****/
+
+#include "Morse.h"
  
 // VARIABLES
+Morse* m;
+char msg[255];
+int mcnt = 0;
+int led = 13;
 
 void setup() {
+  // Initialize the Morse object
+  m = new Morse(led);
+  
+  // Start the serial connection
+  Serial.begin(9600);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    // Collect characters
+    while (Serial.available() > 0) {
+      char temp = Serial.read();
+      if (temp != '\n' && temp != '\r') {
+        if (temp > 96 && temp < 123) { temp -= 32; }
+        if ((temp > 64 && temp < 91) || (temp > 47 && temp < 58) || temp == 32) {
+          if (mcnt == 255) { break; }
+          msg[mcnt] = temp;
+          mcnt++;
+        } 
+      }
+    }
+    
+    // Feed the array in
+    m->say(msg);
+    
+    // Empty the msg array
+    nullMsg();
+  }
+}
+
+void nullMsg() {
+  for(int clr = 0; clr < 255; clr++) {
+    msg[clr] = '\0';
+  }
 }
